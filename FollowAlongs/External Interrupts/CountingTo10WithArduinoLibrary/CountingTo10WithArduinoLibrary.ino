0cef19f2f2244e3b6bdf065aab1b1397fea08c35
yinz1 <yinz1@rose-hulman.edu>
2022-07-19 21:12:25 -0400

    Create CountingTo10WithArduinoLibrary.ino

---
+++ FollowAlongs/External Interrupts/CountingTo10WithArduinoLibrary/CountingTo10WithArduinoLibrary.ino
@@ -0,0 +1,28 @@
+#define PIN_PUSHBUTTON_PRINT 3
+#define PIN_PUSHBUTTON_COUNT 2
+
+volatile uint16_t isrCounter = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Setup");
+  pinMode(PIN_PUSHBUTTON_PRINT, INPUT_PULLUP);
+  pinMode(PIN_PUSHBUTTON_COUNT, INPUT_PULLUP);
+  
+  arrachInterrupt(digitalPinToInterrupt(PIN_PUSHBUTTON_PRINT), pushbuttonISR, FALLING);
+}
+
+void loop()
+{  
+  if(digitalRead(PIN_PUSHBUTTON_PRINT){
+    Serial.println(String("Counter = ") + isrCounter);
+    delay(500);
+  }
+  delay(10);
+}
+
+void pushbuttonISR(){
+  isrCounter++;
+}
+
