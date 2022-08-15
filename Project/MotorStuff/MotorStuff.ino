4025bb9da43cc2561076878865ce32e446bb22ee
rhit-yinz1 <75374376+rhit-yinz1@users.noreply.github.com>
2022-08-15 16:35:57 -0400

    motorWorks

--- Project/MotorStuff/MotorStuff.ino
+++ Project/MotorStuff/MotorStuff.ino
@@ -1,30 +1,34 @@
 // C++ code
 //
-void setup()
-{
+void setup() {
   pinMode(2, INPUT_PULLUP);
   pinMode(12, INPUT_PULLUP);
-	
-  //Outputs for Dubugging LEDs
+
+  // Outputs for Dubugging LEDs
   pinMode(3, OUTPUT);
   pinMode(13, OUTPUT);
 
-  
- //Outputs for Transistors, H-Bridge, ect
-//   pinMode(11, OUTPUT);
-//   pinMode(12, OUTPUT);
-//   pinMode(13, OUTPUT);
+  // Outputs for Transistors, H-Bridge, ect
+  pinMode(11, OUTPUT);  // PWM
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
 }
 
-void loop()
-{
-  //Outputs for debugging LEDs
-  digitalWrite(3, !digitalRead(2));
-  digitalWrite(13, !digitalRead(12));
-  
-  //Outputs for Transistors, H-bridges, etc
-//   digitalWrite(12, !digitalRead(2));
-//   digitalWrite(13, !digitalRead(3));
-//   analogWrite(11,analogRead(A0)/4);
-//   delay(10);
+void loop() {
+  // Outputs for debugging LEDs
+  digitalWrite(10, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(13, LOW);
+  analogWrite(11, 200);
+  if (!digitalRead(2)) {
+    digitalWrite(10, HIGH);
+    digitalWrite(9, LOW);
+    digitalWrite(3, HIGH);
+  } else if (!digitalRead(12)) {
+    digitalWrite(10, LOW);
+    digitalWrite(9, HIGH);
+    digitalWrite(13, HIGH);
+  }
+  delay(10);
 }
